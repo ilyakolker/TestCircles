732cb66cdf348267a5bf7f4d89836df95a790f8b
ilya1989 <ilyakolker@gmail.com>
2018-01-09 21:15:18 +0200

    first commit

---
+++ Profiles.js
@@ -0,0 +1,14 @@
+﻿function Profilebulider() {
+
+    var profiles =
+        [
+            { name: "Avi", Categoty: "Math" },
+            { name: "Yossi", Categoty: "English" },
+            { name: "Ilya", Categoty: "Math" },
+            { name: "Dor", Categoty: "English" },
+            { name: "Benny", Categoty: "Math" },
+            { name: "Amit", Categoty: "English" }
+        ];
+
+    return profiles;
+}
